3ed5a661a2adcb24730f285ac230946218b16d5e
killianL <klecorf@normandiewebschool.fr>
2023-07-26 20:54:08 +0200

    end auth

--- src/middlewares/credentials.middleware.ts
+++ src/middlewares/credentials.middleware.ts
@@ -2,9 +2,9 @@ import { Request, Response, NextFunction } from 'express';
 
 export const corsMiddleware = (req: Request, res: Response, next: NextFunction) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Methods', ' POST'); 
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST'); 
   res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Credentials', 'true'); // Inclure les credentials (cookies) dans les requêtes
+  res.header('Access-Control-Allow-Credentials', 'true');
   if (req.method === 'OPTIONS') {
     res.sendStatus(200);
   } else {
