3c347e879f27f55f1fa5d9b401c79edfd8a00101
killianL <klecorf@normandiewebschool.fr>
2023-12-18 21:35:39 +0100

    comment upload

--- src/middlewares/antivirus.middleware.ts
+++ src/middlewares/antivirus.middleware.ts
@@ -1,43 +1,43 @@
-import { Request, Response, NextFunction } from 'express';
-import fileUpload, { UploadedFile } from 'express-fileupload';
-const ClamScan = require('clamscan');
+// import { Request, Response, NextFunction } from 'express';
+// import fileUpload, { UploadedFile } from 'express-fileupload';
+// const ClamScan = require('clamscan');
 
-const clamscan = new ClamScan();
+// const clamscan = new ClamScan();
 
-export const antivirusMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  if (req.files && req.files.file) {
-    const uploadedFiles: UploadedFile[] | UploadedFile = req.files.file;
+// export const antivirusMiddleware = (req: Request, res: Response, next: NextFunction) => {
+//   if (req.files && req.files.file) {
+//     const uploadedFiles: UploadedFile[] | UploadedFile = req.files.file;
 
-    if (Array.isArray(uploadedFiles)) {
-      for (const uploadedFile of uploadedFiles) {
-        clamscan.isInfected(uploadedFile.data, (err: any, isInfected: any) => {
-          if (err) {
-            console.error('Erreur lors de l\'analyse antivirus :', err);
-            return res.status(500).send('Erreur lors de l\'analyse antivirus.');
-          }
+//     if (Array.isArray(uploadedFiles)) {
+//       for (const uploadedFile of uploadedFiles) {
+//         clamscan.isInfected(uploadedFile.data, (err: any, isInfected: any) => {
+//           if (err) {
+//             console.error('Erreur lors de l\'analyse antivirus :', err);
+//             return res.status(500).send('Erreur lors de l\'analyse antivirus.');
+//           }
 
-          if (isInfected) {
-            return res.status(400).send('Un des fichiers est infecté par un logiciel malveillant.');
-          }
-        });
-      }
+//           if (isInfected) {
+//             return res.status(400).send('Un des fichiers est infecté par un logiciel malveillant.');
+//           }
+//         });
+//       }
 
-      next();
-    } else {
-      clamscan.isInfected(uploadedFiles.data, (err: any, isInfected: any) => {
-        if (err) {
-          console.error('Erreur lors de l\'analyse antivirus :', err);
-          return res.status(500).send('Erreur lors de l\'analyse antivirus.');
-        }
+//       next();
+//     } else {
+//       clamscan.isInfected(uploadedFiles.data, (err: any, isInfected: any) => {
+//         if (err) {
+//           console.error('Erreur lors de l\'analyse antivirus :', err);
+//           return res.status(500).send('Erreur lors de l\'analyse antivirus.');
+//         }
 
-        if (isInfected) {
-          return res.status(400).send('Le fichier est infecté par un logiciel malveillant.');
-        }
+//         if (isInfected) {
+//           return res.status(400).send('Le fichier est infecté par un logiciel malveillant.');
+//         }
 
-        next();
-      });
-    }
-  } else {
-    return res.status(400).send('Aucun fichier n\'a été téléchargé.');
-  }
-};
+//         next();
+//       });
+//     }
+//   } else {
+//     return res.status(400).send('Aucun fichier n\'a été téléchargé.');
+//   }
+// };
